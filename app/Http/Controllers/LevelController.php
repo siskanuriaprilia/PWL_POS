4375480af32389bf8389f10d4d2d173c7e9f05e3
Siska Nuri Aprilia <aprilias878@gmail.com>
2025-02-24 17:01:35 +0700

    prak 4 - 3

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -10,10 +10,14 @@ class LevelController extends Controller
     public function index()
     {
         //DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['cus', 'pelanggan', now()]);
-
         //return 'Insert data baru berhasil';
-        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
-        return 'Update data berhasil. Jumlah data yang diupdate: '.$row. ' baris';
+        
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: '.$row. ' baris';
+
+        $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        return 'Delete data berhasil. Jumlah data yang dihapus: '.$row. ' baris';
+
 
     }
 }
