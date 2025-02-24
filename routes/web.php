cc7066ce2b7eb2d6c4571fb479ffcca15d8f96d4
Siska Nuri Aprilia <aprilias878@gmail.com>
2025-02-24 17:22:04 +0700

    Praktikum 5 - 1

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('level',[LevelController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
