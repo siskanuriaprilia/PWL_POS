2068cd0371fd06c34a615eaa575179ef3aa8cb1e
Siska Nuri Aprilia <aprilias878@gmail.com>
2025-03-03 09:45:26 +0700

    Perbaikan Praktikum 1 langkah 1 dan 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,22 +3,21 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash; // Tambahkan ini
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Data yang akan diupdate
         $data = [
             'level_id' => 2,
             'username' => 'manager_dua',
             'nama' => 'Manager 2',
-            'password' => Hash::make('12345') // Hashing password
+            'password' => Hash::make('12345')
         ];
 
-        // Update data user berdasarkan username
-        UserModel::where('username', 'customer-1')->update($data);
+        // Tambah data user ke database
+        UserModel::create($data);
 
         // Ambil semua data user dari tabel m_user
         $user = UserModel::all();
