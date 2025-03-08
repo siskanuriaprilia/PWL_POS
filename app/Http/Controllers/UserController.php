53ae6d9a68bea405163792d2aaad4ed98bd43c27
Siska Nuri Aprilia <aprilias878@gmail.com>
2025-03-08 13:26:13 +0700

    Praktikum 3 - 5 soale no 2. done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -90,36 +90,53 @@ class UserController extends Controller
 
         // // Tampilkan data user untuk memastikan
 
+        // $user = UserModel::create([
+        //     'username' => 'manage55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+        
+
+        // $user->username = 'manager56';
+        
+        // // Mengecek apakah ada perubahan data
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty(['nama', 'username']); // true
+        
+        // // Mengecek apakah data bersih (tidak ada perubahan)
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
+        
+        // // Menyimpan perubahan data ke database
+        // $user->save();
+        
+        // // Mengecek ulang setelah penyimpanan
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
+
         $user = UserModel::create([
-            'username' => 'manage55',
-            'nama' => 'Manager55',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
         ]);
-        
 
-        $user->username = 'manager56';
-        
-        // Mengecek apakah ada perubahan data
-        $user->isDirty(); // true
-        $user->isDirty('username'); // true
-        $user->isDirty('nama'); // false
-        $user->isDirty(['nama', 'username']); // true
-        
-        // Mengecek apakah data bersih (tidak ada perubahan)
-        $user->isClean(); // false
-        $user->isClean('username'); // false
-        $user->isClean('nama'); // true
-        $user->isClean(['nama', 'username']); // false
-        
-        // Menyimpan perubahan data ke database
+        $user->username = 'manager12';
+
         $user->save();
-        
-        // Mengecek ulang setelah penyimpanan
-        $user->isDirty(); // false
-        $user->isClean(); // true
-        dd($user->isDirty());
 
+        // Mengecek apakah ada perubahan data setelah penyimpanan
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
         
     }
 }  
