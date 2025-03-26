9e3c37092b3f13b57f4992df70a60dec1e533b7d
Siska Nuri Aprilia <aprilias878@gmail.com>
2025-03-26 19:03:46 +0700

    REV Jobsheet 5 - Praktikum 2

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -23,8 +23,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ url('/')}}" class="brand-link">
-      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="{{ url('/') }}" class="brand-link">
+      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
@@ -41,7 +41,6 @@
     <!-- Main content -->
     <section class="content">
         @yield('content')
-
     </section>
     <!-- /.content -->
   </div>
