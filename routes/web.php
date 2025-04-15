bc12cc74a721b1f4b86b6c056c50d5b87128e2f9
Siska Nuri Aprilia <aprilias878@gmail.com>
2025-04-15 17:58:10 +0700

    Jobsheet 8 - B.	Export Excel di Laravel

--- routes/web.php
+++ routes/web.php
@@ -115,6 +115,7 @@ Route::middleware('auth')->group(function () {
         Route::delete('/{id}', [BarangController::class, 'destroy']);
         Route::get('/import', [BarangController::class, 'import']);
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
+        Route::get('/export_excel', [BarangController::class, 'export_excel']); //export excel
     });
 
 });    
